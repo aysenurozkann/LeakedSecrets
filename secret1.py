075194e0203d47459c433131a52e5db695ba9533
unknown <ozkan.aysenur@hotmail.com>
2024-06-28 13:44:27 +0300

    LeakedSecrets

--- secret1.py
+++
@@ -1,6 +0,0 @@
-from typing import Dict
-import aws_lib
-
-def aws_upload(data: Dict):
-database=aws_lib.connect("AKIAIOSFODNN7EXAMPLE","wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
-database.push(data)
